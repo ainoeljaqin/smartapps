8e89144397f33a25e942a95fe5e11d9f64f7b5f4
sams <oteotegodok1945@gmail.com>
2024-10-22 13:10:20 +0700

    remove unused import

--- app/src/main/java/com/example/smartapps/MainActivity.kt
+++ app/src/main/java/com/example/smartapps/MainActivity.kt
@@ -7,12 +7,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
